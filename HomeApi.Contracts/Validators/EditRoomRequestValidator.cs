45ea17b84bac1e68a46fd16a1c007b566f195491
Marat <milya010790@mail.ru>
2024-08-06 22:51:49 +0500

    Добавил ограничение для валидации

--- HomeApi.Contracts/Validators/EditRoomRequestValidator.cs
+++ HomeApi.Contracts/Validators/EditRoomRequestValidator.cs
@@ -12,10 +12,20 @@ namespace HomeApi.Contracts.Validators
     {
         public EditRoomRequestValidator()
         {
-            RuleFor(x=>x.NewName).NotEmpty();
+            RuleFor(x=>x.NewName).NotEmpty().Must(BeSupported)
+                .WithMessage($"Please choose one of the following locations: {string.Join(", ", Values.ValidRooms)}");
             RuleFor(x=>x.NewArea).NotEmpty();
             RuleFor(x=>x.NewVoltage).NotEmpty().ExclusiveBetween(120,260);
             RuleFor(x=>x.NewGasConnected).NotNull();
+        }
+
+        /// <summary>
+        ///  Метод кастомной валидации для свойства location
+        /// </summary>
+        private bool BeSupported(string location)
+        {
+            // Проверим, содержится ли значение в списке допустимых
+            return Values.ValidRooms.Any(e => e == location);
         }
     }
 }
